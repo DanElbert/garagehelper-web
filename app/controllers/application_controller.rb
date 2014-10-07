684a19e715e800c80f44639209b979d647ed025d
Dan Elbert <dan.elbert@gmail.com>
2014-10-07 17:13:29 -0500

    Changed auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with name: "dan", password: "1918"
+  if ENV["GARAGE_HELPER_PASSWORD"]
+    http_basic_authenticate_with name: "dan", password: ENV["GARAGE_HELPER_PASSWORD"]
+  end
 end
