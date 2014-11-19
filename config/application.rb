198d79fd85c8d5f6b704d42e426f76d524dc1bf1
delbert@umn.edu <dan.elbert@gmail.com>
2014-11-19 12:37:32 -0600

    Update config

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,12 @@ module GaragehelperWeb
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    require 'rack/fiber_pool'
+    config.middleware.insert_before Rack::Lock, Rack::FiberPool
+    #config.threadsafe!
+    # Rails::Initializer.run do |config|
+    #
+    # end
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-require 'rack/fiber_pool'
-Rails::Initializer.run do |config|
-  config.middleware.use Rack::FiberPool
-  config.threadsafe!
-end
-
 # Initialize the Rails application.
 Rails.application.initialize!
