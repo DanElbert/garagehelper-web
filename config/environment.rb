67e37c1829887b9bdfbf977e7347c1af68acf8f0
delbert@umn.edu <dan.elbert@gmail.com>
2014-11-19 12:33:01 -0600

    Added rack fiber pool

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+require 'rack/fiber_pool'
+Rails::Initializer.run do |config|
+  config.middleware.use Rack::FiberPool
+  config.threadsafe!
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
